867518da6c512e5a4a245445c21dafc102a24ded
Dragos Tanta <dragos.tanta@gmail.com>
2024-07-23 17:52:03 +0300

    curs40 hamcrest part2

--- src/main/java/tests/HamcrestExample.java
+++ src/main/java/tests/HamcrestExample.java
@@ -3,6 +3,8 @@ package tests;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotEquals;
 
+import java.util.List;
+
 import org.testng.annotations.Test;
 
 import static org.hamcrest.Matchers.*;
@@ -10,6 +12,8 @@ import static org.junit.Assert.assertThat;
 import io.restassured.response.Response;
 import io.restassured.path.json.JsonPath;
 import static io.restassured.RestAssured.given;
+import static utils.NumberChecker.numbersOnly;
+
 
 public class HamcrestExample {
 
@@ -81,6 +85,74 @@ public class HamcrestExample {
 		//https://swapi.dev/api/planets/1/"}
 		assertThat(url, allOf(startsWith("https:"), containsStringIgnoringCase("SWAPI"),
 				endsWith("1/")));
+		
+		
+		List<String> films = json.getList("films");
+		System.out.println(films.get(0));
+		
+		
+		assertThat(films, contains(  
+				"https://swapi.dev/api/films/1/", 
+		        "https://swapi.dev/api/films/3/", 
+		        "https://swapi.dev/api/films/4/", 
+		        "https://swapi.dev/api/films/5/", 
+		        "https://swapi.dev/api/films/6/"));
+	
+		assertThat(films, hasSize(5));
+		assertThat(films, hasSize(lessThan(10)));
+		assertThat(films, hasSize(greaterThan(3)));
+		
+		assertThat(films, both(hasSize(lessThan(6))).and(hasToString(containsString("films/6/"))));
+		
+		
+		assertThat(films, contains(
+				startsWithIgnoringCase("HTTPS:"),
+				containsString("swapi"),
+				equalTo("https://swapi.dev/api/films/4/"),
+				endsWith("/5/"),
+				is(not(equalTo("ABC")))));
+		
+		assertThat(films, hasItem("https://swapi.dev/api/films/4/"));
+		
+		assertThat(films, hasItems("https://swapi.dev/api/films/1/", "https://swapi.dev/api/films/5/"));
+	
+		assertThat(films, hasItem(startsWith("http")));
+		assertThat(films, hasItem(endsWith("/6/")));
+		assertThat(films, hasItem(containsString("swapi.dev")));
+		
+		assertThat(films, hasItems(containsString("swapi.dev"), endsWith("/1/")));
+
+		assertThat(films, is(not(emptyCollectionOf(String.class))));
+	
+		
+		//array
+		String[] array = {json.getString("name"),json.getString("diameter"),
+				json.getString("climate"), json.getString("gravity"), json.getString("terrain")};
+		
+		System.out.println(array[2]);
+		
+		assertThat(array, arrayContaining("Tatooine", "10465" ,"arid","1 standard", "desert" ));
+		
+		assertThat(array, arrayContainingInAnyOrder("10465","Tatooine" ,"arid","1 standard", "desert" ));
+		
+		assertThat(array, is(not(nullValue())));
+		
+		
+		String orbitalPeriod = json.getString("orbital_period");
+		String climate = json.getString("climate");
+		String gravity2 = json.getString("gravity");
+	
+		System.out.println("--------------------------------------");
+		System.out.println(orbitalPeriod);
+		System.out.println(climate);
+		System.out.println(gravity2);
+		
+		
+		
+		assertThat(orbitalPeriod, is(numbersOnly()));
+		assertThat(climate, is(not(numbersOnly())));
+		assertThat(gravity2, is(not(numbersOnly())));
+
 		
 	}
 	
